df178267a4e4fd4398e0ad5eb4ead784fd65d6c9
Adarsh Kumar Singh <adarshsinghrathore3.com>
2022-03-19 16:59:42 +0530

    added db and made some chnges in templates

--- base/views.py
+++ base/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+rooms = [
+    {'id':1, 'name':"Let's Learn Python!"},
+    {'id':2, 'name':"Let's Learn React JS!"},
+    {'id':3, 'name':"Let's Learn Data Engineering!"}
+]
+
 # Create your views here.
 def home(request):
-    return HttpResponse("Home Page")
+    return render(request, 'base/home.html', {'rooms': rooms})
 
-def room(request):
-    return HttpResponse("Room")
+def room(request, pk):
+    print(pk)
+    room = None
+    for i in rooms:
+        if i["id"] == int(pk):
+            room = i 
+    context = {"room":room}
+    return render(request, 'base/room.html', context)
